e29f080b722e9ea6ae83dd4f86a5ab406c4fff60
KU Lok Sun <sundaykuloksun@gmail.com>
2018-04-13 18:00:36 +0800

    Move the promise into test suit
    If we create the problem before test run, the bluebird will throw unhandled
    rejection even we defined .catch() later.

--- index.js
+++ index.js
@@ -9,11 +9,25 @@ const { getResolvePromise, getRejectPromise } = require('./service.js');
 
 describe('.then()', () => {
   context('when promise resolved', () => {
-    const promise = getResolvePromise();
-    const callback = sinon.stub();
     it('should trigger callback once', async () => {
-      await promise.then(callback);
+      const callback = sinon.stub();
+      const promise = getResolvePromise()
+        .then(callback);
+      await promise;
       expect(callback).to.have.been.calledOnce;
+    });
+  });
+
+  context('when promise rejected', () => {
+    it('should trigger errorCallback and callback will be skipped', async () => {
+      const callback = sinon.stub();
+      const errorCallback = sinon.stub();
+      const promise = getRejectPromise()
+        .then(callback)
+        .catch(errorCallback);
+      await promise;
+      expect(callback).to.have.callCount(0);
+      expect(errorCallback).to.have.been.calledOnce;
     });
   });
 });
