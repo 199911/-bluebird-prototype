c4d9b76329bc9e279bef5585c273881802e15895
KU Lok Sun <sundaykuloksun@gmail.com>
2018-04-17 14:59:40 +0800

    Add reject case for then catch then catch chain

--- index.js
+++ index.js
@@ -82,7 +82,26 @@ describe('.then().catch().then().catch()', () => {
       const [firstCallback, secondCallback, firstErrorCallback, secondErrorCallback] = callbacks;
       await getPromise(getResolvePromise(), callbacks);
       expect(firstCallback).to.have.been.calledOnce;
+      expect(firstErrorCallback).to.have.callCount(0);
       expect(secondCallback).to.have.been.calledOnce;
+      expect(secondErrorCallback).to.have.callCount(0);
+    });
+  });
+  context('when promise rejected', () => {
+    let callbacks;
+    before('set up promise', async () => {
+      callbacks = [];
+      for (let i = 0; i < 4; ++i) {
+        callbacks.push(sinon.spy());
+      }
+    });
+    it('should trigger first error callback and second callback', async () => {
+      const [firstCallback, secondCallback, firstErrorCallback, secondErrorCallback] = callbacks;
+      await getPromise(getRejectPromise(), callbacks);
+      expect(firstCallback).to.have.callCount(0);
+      expect(firstErrorCallback).to.have.been.calledOnce;
+      expect(secondCallback).to.have.been.calledOnce;
+      expect(secondErrorCallback).to.have.callCount(0);
     });
   });
 });
